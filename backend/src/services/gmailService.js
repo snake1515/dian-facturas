9d4a63e2974f63da01b4a6578467b7d7c7f09958
snake1515 <regarcia1515@gmail.com>
2026-05-26 22:18:29 -0500

    fix: agregar scope gmail.send al auth

--- backend/src/services/gmailService.js
+++ backend/src/services/gmailService.js
@@ -19,6 +19,7 @@ const getAuthUrl = () => {
     prompt: 'consent',
     scope: [
       'https://www.googleapis.com/auth/gmail.readonly',
+      'https://www.googleapis.com/auth/gmail.send',
       'https://www.googleapis.com/auth/userinfo.email',
     ],
   });
