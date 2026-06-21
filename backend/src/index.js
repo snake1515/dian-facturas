7d021b3437dc16ce5cbc1bf4845194f925b548a9
snake1515 <regarcia1515@gmail.com>
2026-06-20 22:37:45 -0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,7 @@ app.use(helmet({
 app.use(cors({
   origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
@@ -135,6 +135,35 @@ const arrancar = async () => {
 };
 
 arrancar();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
